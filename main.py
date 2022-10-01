432677368f2d6b3083d262ee3fac002d40de8027
Paulina Jabłońska <hello@aylin.dev>
2022-10-01 21:30:32 +0200

    Write requested jokes to file

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 from fastapi import FastAPI
 import requests
 from bs4 import BeautifulSoup
+import pickle
 import pprint
 
 app = FastAPI()
 # scrape site
 URL = "http://bash.org.pl/latest/?page="
 # take first 5 pages, it should be enough to get 100 jokes
-req = []
 for page in range(1, 6):
-    req.append(requests.get(URL + str(page)))
+    file = (requests.get(URL + str(page)))
+    # it would be nice to handle errors ie when page is unavailable
     # req.raise_for_status()
 
-# parse HTML page, sift through it and get the content we need
+    # save scraped pages to file, it's not nice to overload someone's page with requests
+    # for now this doesn't check if file already has proper content!
+    try:
+        f = open("jokes.html", "ab")
+        pickle.dump(file, f)
+        f.close()
+    except FileNotFoundError:
+        with open("joke.html", "wb") as f:
+            pickle.dump(file, f)
+        f.close()
+
+# parse saved pages, sift through them and get the content we need
 jokes_list = []
-for page in req:
-    soup = BeautifulSoup(page.text, "html.parser")
+with open("jokes.html", "rb") as file:
+    soup = BeautifulSoup(file, "html.parser")
     jokes_dirty_list = soup.find_all('div', class_="quote post-content post-body")
     for j in jokes_dirty_list:
         jokes_list.append(j.text)
+    file.close()
 
 pprint.pprint(jokes_list)
 
